c2634765a528093c7acafb7d4ea64fda1df86f82
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-12-29 18:29:50 -0500

    day 9

--- react-mern-app/src/pages/Home/Home.js
+++ react-mern-app/src/pages/Home/Home.js
@@ -1,50 +1,119 @@
+import React, { useRef, useState } from 'react'
 
 
-// Dependencies
+function CRUD() {
+  const post = [
+    {
+      id: 1,
+      title: "Deep Thoughts",
+      body: "Friends, I am the realest coder alive",
+      author: "Arthur",
+    },
+    {
+      id: 2,
+      title: "Sage Advice",
+      body: "Friends, I am awesome and you are too",
+      author: "Arthur",
+      likes: 20,
+    },
+    {
+      id: 3,
+      title: "Is TI the Jadakiss of the South",
+      body: "TI is severely underrated and we need to fix that expeditiously",
+      author: "Arthur",
+      likes: 40,
+    },
 
-
-
-export default function Main({data}) {
-  console.log(data)
-
-
-  
+  ]
+  const [posts, setPost] = useState(post)
+  const [updateState, setUpdateState] = useState(-1)
   return (
-    <>
-  
- 
-
-    </>
+    <div className='crud'>
+      <div>
+        <Create setPost={setPost} />
+        <form onSubmit={handleUpdate}>
+          <table>
+            {
+              posts.map((current) => (
+                updateState === current.id ? <Edit current={current} posts={posts} setPost={setPost} /> :
+                  <tr>
+                    <td>{current.title}</td>
+                    <td>{current.body}</td>
+                    <td>
+                      <button className='edit' onClick={() => handleEdit(current.id)} type="button">Edit</button>
+                      <button className='delete' onClick={() => handleDelete(current.id)} type="button">Delete</button>
+                    </td>
+                  </tr>
+              ))
+            }
+          </table>
+        </form>
+      </div>
+    </div>
   )
-};
+
+  function handleUpdate(event) {
+    event.preventDefault()
+    setUpdateState(-1)
+
+  }
+  function handleEdit(id) {
+    setUpdateState(id)
+  }
+  function handleDelete(id) {
+    const newPost = posts.filter((li) => li.id !== id)
+    setPost(newPost)
+  }
+
+}
+
+function Edit({ current, posts, setPost }) {
+  function handleInput(event) {
+    const newPost = posts.map(li => (
+      li.id === current.id ? { ...li, [event.target.name]: event.target.value } : li
+    ))
+
+    setPost(newPost)
+  }
+
+  return (
+    <tr>
+      <td><input type="text" name='title' onChange={handleInput} value={current.title} /></td>
+      <td><input type="text" name='body'onChange={handleInput} value={current.body} /></td>
+      <td><button type='submit'>Update</button></td>
+    </tr>
+  )
+}
 
 
+function Create({ setPost }) {
+  const titleRef = useRef()
+  const bodyRef = useRef()
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const title = event.target.elements.title.value;
+    const body = event.target.elements.body.value;
+    const newPost = [
+      {
+        id: 4,
+        title,
+        body
+      },
+    ]
+    setPost((prevPost) => {
+      return prevPost.concat(newPost)
+    })
+    titleRef.current.value = ""
+    bodyRef.current.value = ""
+  }
+  return (
+    <form className='create' onSubmit={handleSubmit}>
+      <input type="text" name="title" placeholder="Enter Title" ref={titleRef} />
+      <input type="text" name="body" placeholder="Enter Body" ref={bodyRef} />
+      <button type="submit">Create Post</button>
+    </form>
+  )
+}
 
-// CHEAT-SHEET
-
-/*
-CREATE
-  .create(data, callback function(error, new doc){})
-  .insertMany([data], callback function(error, new docs){})
-
-READ
-  .find({filter}, callback function(err, docs){})
-  .findOne({filter}, callback function(err, doc){})
-  .findById("id", callback function(err, doc){})
-
-UPDATE
-  .findByIdAndUpdate("id", data, {new:true}, callback function)
-  .updateOne({filter}, data, {new:true, multi: true}, callback function)
-
-DELETE
-  this is the method you should use when delete
-  .findByIdAndDelete("id", callback function(error, deleted doc){})
-
-  another one you COULD use
-  .deleteOne({filter}, callback function(error, deleted doc){})
-
-  only use this IF you know exactly what you are doing
-  .deleteMany({filter}, callback function(error, deleted docs){})
-
-*/
+export default CRUD;
